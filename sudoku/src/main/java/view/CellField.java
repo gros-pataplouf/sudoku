58094b8443bef84e0df680736af34fdef35fa2ea
gros-pataplouf <sceres@pm.me>
2024-07-28 11:41:00 +0200

    add cellfield

---
+++ sudoku/src/main/java/view/CellField.java
@@ -0,0 +1,14 @@
+package view;
+
+import javax.swing.JTextField;
+
+import model.Cell;
+
+public class CellField extends JTextField{
+    public CellField(Cell cell) {
+        String value = String.valueOf(cell.getDisplayValue());
+        setText(value);
+        
+    }
+
+}
