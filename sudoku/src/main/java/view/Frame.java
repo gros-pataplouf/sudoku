fb68b374dcf455aeffdbe638f9ade395bf976017
gros-pataplouf <sceres@pm.me>
2024-07-30 20:22:46 +0200

    draw the borders

--- sudoku/src/main/java/view/Frame.java
+++ sudoku/src/main/java/view/Frame.java
@@ -10,12 +10,12 @@ import model.Board;
 import model.Cell;
 
 
+
 public class Frame extends JFrame {
     GridLayout grid = new GridLayout(9, 9);
     Frame() {
         setTitle("Sukoku");
         setSize(540, 540);
-        setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     }
@@ -41,15 +41,21 @@ public class Frame extends JFrame {
                 if (currentCell.isShown()) {
                     cellField.setEnabled(false);
                 }
+                int topThickness = i == 0 ? 3 : 1;
+                int leftThickness =  j % 3 == 0 ? 3 : 1;
+                int bottomThickness = (i + 1) % 3 == 0 ? 3 : 1;
+                int rightThickness = j == 8 ? 3 : 1;
+                cellField.setBorder(BorderFactory.createMatteBorder(topThickness, leftThickness, bottomThickness, rightThickness, Color.BLACK));
                 panel.add(cellField);
             }
         }
+
         this.add(panel);
     }
 
-    // main method
-    public static void main(String argvs[]) {
+      public static void main(String argvs[]) {
         Frame frame = new Frame();
         frame.createGrid(9, 9);
+        frame.setVisible(true);
     }
 }
